c6af57b157c1bea45df04421de3e16ff385b74b3
Tien Pham <pvtienlamk@gmail.com>
2020-05-16 23:07:39 +0700

    Update activity3a4

--- src/buoi3/Activity3a4.java
+++ src/buoi3/Activity3a4.java
@@ -2,6 +2,17 @@ package buoi3;
 
 public class Activity3a4 {
     public static void main(String[] args) {
+        String s = "Hello student in PlusPlus academy";
+        String[] word = s.split(" ");
+        String longestword = " ";
+        for (int i = 0; i < word.length; i++) {
+            for (int j = i +1; j <word.length ; j++) {
+                if (word[i].length() >= word[j].length()); {
+                    longestword = word[i];
+                }
+            }
+        }
+        System.out.println(longestword);
 
     }
 }
